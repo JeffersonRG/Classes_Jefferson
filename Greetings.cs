e17893b5bc7783ca8fb17ad5cd5f46afbe612d3d
JeffersonRG <jgutierez500@gmail.com>
2023-04-14 14:13:30 -0500

    Initial commit

---
+++ Greetings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Jefferson
+{
+    class Greetings
+    {
+        /// <summary>
+        /// Prints a greeting to the user
+        /// </summary>
+        public void Welcome()
+        {
+            Console.WriteLine("Hello Traveler, welcome!");
+        }
+
+        /// <summary>
+        /// Print out a welcome message with the users name
+        /// </summary>
+        /// <param name="name">user's name</param>
+        public void Hello(string name)
+        {
+            Console.WriteLine($"{name}, thank you for joining us today!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Classes_Jefferson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //links the Greetings.cs
+            Greetings greetings = new Greetings();
+            //Welcomes the user
+            greetings.Welcome();
+
+            //Asked to use it for the name and assigns it to userName
+            Console.WriteLine("what is your name squire?");
+            String userName = Console.ReadLine();
+
+            //Grease to user with what name they inputted
+            greetings.Hello(userName);
+        }
+    }
+}
